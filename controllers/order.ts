68bf0eddd418a4aedd928c78f6b3e74dfeef5c59
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-06-02 16:53:44 +0100

    updates: user can upadte order.

--- controllers/order.ts
+++ controllers/order.ts
@@ -11,7 +11,7 @@ import {
 import { Router, Request, Response, query } from 'express';
 import { AuthenticatedRequest, IControllerBase, RequestType } from '../shared/types';
 import { requireRole, upload, userAuth } from '../shared/middlewares';
-import { PrismaClient, User, user_role} from '@prisma/client';
+import { PrismaClient, User, order_status, user_role } from '@prisma/client';
 
 export default class OrderController implements IControllerBase {
 
@@ -30,10 +30,14 @@ export default class OrderController implements IControllerBase {
             this.getCompanyOrders)
 
         this.router.get("/:orderId", this.getOrder)
+            .patch("/:orderId", [userAuth, requireRole([user_role.vendor,
+            user_role.company])], this.updateOrder)
 
         this.router.post('/place-order',
             [userAuth, requireRole([user_role.company])],
             this.placeOrder)
+
+        // this.router.patch()
     }
 
 
@@ -98,17 +102,17 @@ export default class OrderController implements IControllerBase {
     getCompanyOrders = async (req: AuthenticatedRequest, res: Response) => {
         try {
             const companyId = req?.params?.companyId;
-            const status = req?.query?.status as string;
+            const status = req?.query?.status as order_status;
             const filter = {};
 
             if (status) {
-                const statusValue = status === "pending" ? false : status === "success" ? true : undefined;
+                const statusValue = (status === order_status.pending || status === order_status.delivered) ? status : undefined;
                 if (statusValue === undefined) {
                     return res.status(400).json({ message: "Something went wrong!" })
                 }
 
                 //@ts-ignore
-                filter["vendorDelivered"] = statusValue;
+                filter["order_status"] = statusValue;
             }
 
             if (req.user?.role === user_role.company || req?.user?.role === user_role.vendor) {
@@ -141,6 +145,9 @@ export default class OrderController implements IControllerBase {
         try {
             const orderId = req?.params?.orderId
             const result = await this.prisma.order.findFirst({ where: { id: orderId } })
+            if (!result) {
+                return res.status(404).json({ message: "Order Not Found!" })
+            }
             return res.status(201).json({ result })
         } catch (error) {
             return res.status(500).json(errorMessage(error))
@@ -149,12 +156,32 @@ export default class OrderController implements IControllerBase {
 
     updateOrder = async (req: AuthenticatedRequest, res: Response) => {
         try {
-            const orderId = req?.params?.orderId
-            const result = await this.prisma.order.findFirst({ where: { id: orderId } })
+            const orderId = req?.params?.orderId;
+
+            const result = await this.prisma.order.findFirst({
+                where: {
+                    id: orderId, 
+                    companyId: req?.user?.companyId
+                }
+            })
 
             if (!result) {
                 return res.status(404).json({ message: "Order Not Found!" })
             }
+
+            const orderStatus = req?.body?.orderStatus as order_status;
+
+            if ((orderStatus !== order_status.cancelled) && (orderStatus !== order_status.delivered)) {
+                return res.status(400).json({ message: "Something went wrong!" })
+            }
+
+
+            await this.prisma.order.update({
+                where: { id: orderId },
+                data: { order_status: orderStatus }
+            })
+
+            return res.status(200).json({ message: "Update order status successfully!" })
         } catch (error) {
             return res.status(500).json(errorMessage(error))
         }
