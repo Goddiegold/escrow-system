9e0a2d70c0b021974d71a87ff0fd36a0e8154c9f
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-07-09 11:41:49 +0100

    updates: worked on notifications

--- src/pages/dashboard/NotificationsPage.tsx
+++ src/pages/dashboard/NotificationsPage.tsx
@@ -1,53 +1,101 @@
 import { useUserContext } from "@/context/UserContext";
-import { useClient } from "@/shared/client";
+import client, { useClient } from "@/shared/client";
 import { convertAmount, getInitials, toast } from "@/shared/helpers";
 import { notification_type, NotificationType, User } from "@/shared/types";
-import { ActionIcon, Avatar, Button, Center, Flex, Paper, Skeleton, Space, Spoiler, Text, Tooltip } from "@mantine/core";
+import {
+    ActionIcon, Avatar, Button, Center, Flex, Paper,
+    Skeleton, Space, Spoiler, Text, Tooltip
+} from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 import { ArrowsClockwise, Check } from "@phosphor-icons/react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import moment from "moment";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface NotificationItemProps {
-    customer: User | null,
-    createdAt: Date | null,
+    item: NotificationType,
     message: string | null,
+    customer: User | null,
+    path: string | null,
 }
 
 const NotificationItem: React.FC<NotificationItemProps> = ({ ...props }) => {
-    const { createdAt, customer, message } = props;
+    const { createdAt, id: notificationId } = props.item;
+    const { user } = useUserContext()
+    const queryClient = useQueryClient()
+    const [loading, setLoading] = useState(false)
+    const { width } = useViewportSize()
+    const navigate = useNavigate()
+
+    // const proceedBtnVisible = 
+
+    const handleMarkAsRead = async () => {
+        try {
+            setLoading(true)
+            const res = await client().put(`/users/notifications/${notificationId}`)
+            queryClient.setQueryData(["notifications", user?.id], (data: NotificationType[] | null) => {
+                if (!data) return;
+                return data.filter(item => item.id !== notificationId)
+            })
+            setLoading(false)
+            toast(res?.data?.message).success()
+        } catch (error) {
+            toast(error?.response?.data?.message).error()
+        }
+    }
+
+    const biggerScreen = width >= 768
+
     return (
         <Paper shadow="sm"
-            maw={500} mb={10} p={"md"}>
-            <Flex className="flex-1" justify={"space-between"}
-                wrap={"wrap"} direction={"row"}>
+            radius={"md"}
+            maw={500} mb={10} p={"sm"}>
+            <Flex className="flex-1"
+                justify={"space-between"}
+                wrap={biggerScreen ? "nowrap" : "wrap"}
+                direction={"row"}>
                 <Flex direction={"row"}>
                     <Flex>
-                        <Avatar w={50} h={50}>{customer ? getInitials(customer.name) : "EA"}</Avatar>
+                        <Avatar w={50} h={50}>{props?.customer ? getInitials(props.customer.name) : "EA"}</Avatar>
                     </Flex>
                     <Space mx="xs" />
                     <Flex direction={"column"}>
-                        <Spoiler maxHeight={50} showLabel="Show more" hideLabel="Hide">
-                            <Text>{message}</Text>
+                        <Spoiler maxHeight={50} showLabel="Show more" hideLabel="Hide" classNames={{
+                            control: "!text-xs"
+                        }}>
+                            <Text fw={300}>{props.message}</Text>
                             <Text c={"dimmed"} fz="xs">{moment(createdAt || new Date()).fromNow()}</Text>
                         </Spoiler>
                     </Flex>
                 </Flex>
 
-                <Tooltip label="Mark as read">
-                    <ActionIcon variant="transparent" color="green">
-                        <Check size={20} /></ActionIcon>
-                </Tooltip>
+
+                <Flex
+                    w={!biggerScreen ? "100%" : "15%"}
+                    justify={width >= 768 ? "flex-end" : "space-between"}
+                    align={"flex-end"}
+                    direction={biggerScreen ? "column" : "row-reverse"}>
+                    <Tooltip label="Mark as read">
+                        <ActionIcon variant="transparent"
+                            color="green" onClick={handleMarkAsRead}
+                            loading={loading}>
+                            <Check size={20} /></ActionIcon>
+                    </Tooltip>
+                    <Button variant="transparent" p={0} size="xs" onClick={() => navigate(props.path)}>Proceed</Button>
+                </Flex>
             </Flex>
-        </Paper>
+        </Paper >
     )
 }
 
 const NotificationsPage = () => {
     const { user } = useUserContext()
     const clientInstance = useClient()
+    const queryKey = ["notifications", user?.id]
 
     const { data, isLoading, refetch: refetchNotifications, isRefetching } = useQuery({
-        queryKey: ["notifications", user?.id],
+        queryKey,
         queryFn: async () => {
             try {
                 const response = await clientInstance().get("/users/notifications")
@@ -62,19 +110,23 @@ const NotificationsPage = () => {
 
     const isFetching = isLoading || isRefetching;
 
+
     const renderNotificationItems = (items: NotificationType[]) => {
         return items.map((item) => {
-            const { type: notificationType, order, createdAt } = item;
+            const { type: notificationType, order } = item;
             const customer = order?.customer;
             const orderRef = order?.orderRef
             let message = "";
+            let path = "#";
 
             switch (notificationType) {
                 case notification_type.customer_placed_order:
                     message = `${customer?.name || 'A customer'} just placed an order on your store. Order reference: ${orderRef}`;
+                    path = "/dashboard/orders"
                     break;
                 case notification_type.delivery_confirmed:
-                    message = `${customer?.name || 'A customer'} just confirmed that order ${orderRef} ${order?.userReceived ? "has been received successfully" : "has been received"}.${order?.userReceived && ` You've been credited with ₦${convertAmount(order.totalAmount)}`}`;
+                    message = `${customer?.name || 'A customer'} just confirmed that order ${orderRef} ${order?.userReceived ? "has been received successfully" : "hasn't been received"}.${order?.userReceived && ` You've been credited with ₦${convertAmount(order.totalAmount)}`}`;
+                    path = order?.userReceived ? "/dashboard/wallet/" : "/dashboard/orders"
                     break;
                 default:
                     message = "You have a new notification.";
@@ -83,9 +135,10 @@ const NotificationsPage = () => {
 
             return (
                 <NotificationItem
-                    createdAt={createdAt}
-                    customer={customer}
                     message={message}
+                    item={item}
+                    path={path}
+                    customer={customer!}
                     key={item.id}
                 />
             );
@@ -111,7 +164,7 @@ const NotificationsPage = () => {
                     <>
                         {renderNotificationItems(data)}
                     </> : <Center>
-                        <Text>No notifications found</Text>
+                        <Text c={"dark"}>No notifications found</Text>
                     </Center>
                 }
             </> : <Flex direction={"column"}>
