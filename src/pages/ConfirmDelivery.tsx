18b75617544cec721447167a37c86ce1bb49fcc7
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2024-07-13 19:26:14 +0100

    updates: updated the design for confirm and order payment page.

--- src/pages/ConfirmDelivery.tsx
+++ src/pages/ConfirmDelivery.tsx
@@ -50,79 +50,88 @@ const ConfirmDelivery = () => {
 
   return (
     <Flex direction={"column"} h={"100vh"}>
-      <LoadingOverlay visible={loading1} zIndex={1000}
-        overlayProps={{ radius: "sm", blur: 2 }} />
-      <Flex
-        align={'center'}
-        maw={600}
-        mx={"auto"}
-        my={"auto"}
-        direction={'column'}
-        justify={'center'} mah={'100vh'}
-        className="border shadow-sm rounded-3xl"
-        p={20}>
-        <Logo />
-
-        {currentOrder
-          && !currentOrder?.userReceived ? <Flex my={20} direction={'column'} w={"95%"}>
-          <Text fw={500} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
-          <Text fw={400}
-          >
-            Please confirm if your order {currentOrder.orderRef} with
-            {currentOrder?.company?.name} has been delivered by {currentOrder?.vendor?.name}.</Text>
-          <Flex my={10} direction={"column"}>
-            <Rating
-              fractions={4}
-              value={rating.value}
-              size="xl" my={10}
-              onChange={value => setRating({ ...rating, value })} />
-            <Textarea
-              className="w-full"
-              minRows={5} maxRows={7}
-              my={10}
-              value={rating.review}
-              onChange={({ target }) =>
-                setRating({
-                  ...rating,
-                  review: target.value
-                })} />
-          </Flex>
-
-          <Flex className="flex flex-col sm:flex-row" my={10} justify={"space-between"}>
-            <Button color="dark" fw={400} className="mb-[10px] sm:mb-0">No (I haven't received the order)</Button>
-            <Popover width={200} position="bottom" withArrow shadow="md">
-              <Popover.Target>
-                <Button color="red" fw={400}>Yes (I have received the order)</Button>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <Text size="sm">Are you sure you've received the delivery?</Text>
-                <Flex justify={"space-between"} my={5}>
-                  <Button color="black" size="xs"
-                    loading={loading2}
-                    onClick={() => handleConfimation(false)}>No</Button>
-                  <Button color="red" size="xs"
-                    loading={loading2}
-                    onClick={() => handleConfimation(true)}>Yes</Button>
+      <Flex direction={"column"} mx={"auto"}
+        my={"auto"}>
+        <Flex
+          align={'center'}
+          maw={600}
+          direction={'column'}
+          className="border shadow-sm rounded-3xl"
+          p={20}>
+          {loading1 ? <LoadingOverlay zIndex={1000}
+            visible
+            overlayProps={{ radius: "sm", blur: 2 }} /> :
+            <>
+              <Logo />
+              {currentOrder
+                && !currentOrder?.userReceived ? <Flex my={20} direction={'column'} w={"95%"}>
+                <Text fw={500} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
+                <Text fw={400}
+                >
+                  Please confirm if your order {currentOrder.orderRef} with
+                  {currentOrder?.company?.name} has been delivered by {currentOrder?.vendor?.name}.</Text>
+                <Flex my={10} direction={"column"}>
+                  <Rating
+                    fractions={4}
+                    value={rating.value}
+                    size="xl" my={10}
+                    onChange={value => setRating({ ...rating, value })} />
+                  <Textarea
+                    className="w-full"
+                    minRows={5} maxRows={7}
+                    my={10}
+                    value={rating.review}
+                    onChange={({ target }) =>
+                      setRating({
+                        ...rating,
+                        review: target.value
+                      })} />
                 </Flex>
-              </Popover.Dropdown>
-            </Popover>
-          </Flex>
-        </Flex> :
-          <Flex my={20} direction={'column'}>
-            <Text fw={500} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
-            <Flex direction={"column"} my={20}>
-              <CheckCircle size={100} color="#228be6" className="mx-auto" />
-              <Text my={10}>This Order has been confirmed successfully, if you didn't confirm it contact support.</Text>
-              <Flex justify={"flex-start"}>
-                <Button
-                  variant="transparent"
-                  p={0} mx={0}>
-                  support@escrowsystem.com
-                </Button>
-              </Flex>
-            </Flex>
-          </Flex>}
 
+                <Flex className="flex flex-col sm:flex-row" my={10} justify={"space-between"}>
+                  <Button color="dark" fw={400} className="mb-[10px] sm:mb-0">No (I haven't received the order)</Button>
+                  <Popover width={200} position="bottom" withArrow shadow="md">
+                    <Popover.Target>
+                      <Button color="red" fw={400}>Yes (I have received the order)</Button>
+                    </Popover.Target>
+                    <Popover.Dropdown>
+                      <Text size="sm">Are you sure you've received the delivery?</Text>
+                      <Flex justify={"space-between"} my={5}>
+                        <Button color="black" size="xs"
+                          loading={loading2}
+                          onClick={() => handleConfimation(false)}>No</Button>
+                        <Button color="red" size="xs"
+                          loading={loading2}
+                          onClick={() => handleConfimation(true)}>Yes</Button>
+                      </Flex>
+                    </Popover.Dropdown>
+                  </Popover>
+                </Flex>
+              </Flex> :
+                <Flex my={20} direction={'column'}>
+                  <Text fw={500} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
+                  <Flex direction={"column"} my={20}>
+                    <CheckCircle size={100} color="#228be6" className="mx-auto" />
+                    <Text my={10}>This Order has been confirmed successfully, if you didn't confirm it contact support.</Text>
+                    <Flex justify={"flex-start"}>
+                      <Button
+                        variant="transparent"
+                        p={0} mx={0}>
+                        support@escrowsystem.com
+                      </Button>
+                    </Flex>
+                  </Flex>
+                </Flex>}
+            </>
+          }
+
+        </Flex>
+
+        {!loading1 && <Flex
+          mx={"auto"}
+          maw={500} my={10} p={20}>
+          <Text fw={300} size="sm">24/7 support via support@escrowsystem.com or +234 705 215 2823</Text>
+        </Flex>}
       </Flex>
     </Flex>
   );
--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,16 +1,22 @@
 import Logo from "@/components/shared/Logo";
-import { useClient } from "@/shared/client";
+import client, { useClient } from "@/shared/client";
 import { calculateServiceFee, toast } from "@/shared/helpers";
-import { Order } from "@/shared/types";
-import { Button, Divider, Flex, NumberFormatter, Text } from "@mantine/core";
+import { Button, Divider, Flex, LoadingOverlay, NumberFormatter, Text } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import { v4 as uuidv4 } from 'uuid';
 import { useNavigate, useParams } from "react-router-dom";
+import { PaystackConsumer } from "react-paystack";
+import { Order } from "@/shared/types";
+import { useState } from "react";
+import { CheckCircle } from "@phosphor-icons/react";
 
 const PaymentPage = () => {
     const { orderRef } = useParams()
     const clientInstance = useClient()
     const navigate = useNavigate()
+    const paymentRef = `orderPayment_${uuidv4()}`;
     console.log('Order Reference:', orderRef);
+    const [loading, setLoading] = useState(false)
 
     const { data,
         isLoading } = useQuery({
@@ -18,7 +24,7 @@ const PaymentPage = () => {
             queryFn: async () => {
                 try {
                     const res = await clientInstance().get(`/orders/order-ref/${orderRef}`)
-                    return res?.data?.result as { totalAmount: number }[];
+                    return res?.data?.result as Order[];
                 } catch (error) {
                     toast(error?.response?.data?.message).error()
                     navigate("/not-found")
@@ -29,68 +35,148 @@ const PaymentPage = () => {
         })
 
     const orderPrice = data ? data.reduce((acc: number, item) => {
-        return acc + item.totalAmount;
+        return acc + item!.totalAmount;
     }, 0) : 0;
 
     const serviceCharge = calculateServiceFee(orderPrice)
 
     const totalAmount = serviceCharge + orderPrice;
 
+    const config = {
+        reference: paymentRef,
+        email: data ? data[0]?.customer?.email : null,
+        name: data ? data[0]?.customer?.name : null,
+        amount: totalAmount * 100,
+        publicKey: 'pk_test_6cf85813bd75ff7ae5c04a8f109a174b9093be3c',
+    };
+
+    const handlePayment = async () => {
+        try {
+            setLoading(true)
+            const res = await client().get(`/orders/init-payment/${orderRef}?transactionRef=${paymentRef}`)
+            const transactionRef = res?.data?.result?.transactionRef as string;
+            return transactionRef;
+        } catch (err) {
+            setLoading(false)
+            toast(err?.response?.data?.message, "Contact Support Support!").error()
+        }
+    }
+
+    const paystackConsumerProps = {
+        ...config,
+        onSuccess: (response) => {
+            console.log(response)
+            //verify payment
+            client().get(`/orders/verify-payment/${orderRef}?transactionRef=${response?.reference}`)
+                .then(res => {
+                    setLoading(false)
+                    toast("You would be contacted by the event planner soon!",
+                        'Payment Successful!').success()
+                }).catch(err => {
+                    setLoading(false)
+                    toast(err?.response?.data?.message, "Contact Support Support!").error()
+                })
+        },
+        onClose: () => {
+            setLoading(false)
+            toast("Payment failed!").error()
+        }
+
+    }
+
     return (
-        <Flex direction={"column"} h={"100vh"}>
-            <Flex
-                align={'center'}
-                maw={500}
-                mx={"auto"}
-                my={"auto"}
-                // my={20}
-                direction={'column'}
-                className="border shadow-sm rounded-3xl"
-                justify={'center'} mah={'100vh'}
-                p={20}>
-                <Logo />
-                <Flex my={20} direction={'column'}>
-                    <Text fw={600} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
-                    <Text fz={"md"}>Make payment to complete the process of your order. Order ID - #{orderRef}</Text>
-                    <Flex justify={"space-between"} my={10} direction={"column"}>
+        <Flex direction={"column"} h={"100vh"} >
+            <Flex direction={"column"} mx={"auto"}
+                my={"auto"}>
+                <Flex
+                    align={"center"}
+                    maw={500}
+                    direction={'column'}
+                    className="border shadow-sm rounded-3xl"
+                    p={20}>
+                    {isLoading ? <LoadingOverlay visible zIndex={1000} w={"100%"}
+                        overlayProps={{ radius: "sm", blur: 2 }} /> :
+                        <>
+                            <Logo />
+                            {data && !data[0]?.userPaid ?
+                                <Flex my={20} direction={'column'}>
+                                    <Text fw={600} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
+                                    <Text fz={"md"}>Make payment to complete the process of your order. Order ID - #{orderRef}</Text>
+                                    <Flex justify={"space-between"} my={10} direction={"column"}>
 
-                        <Flex justify={"space-between"}>
-                            <Text size="sm">Original cost:  </Text>
-                            <NumberFormatter
-                                className="text-sm"
-                                thousandSeparator
-                                prefix="₦"
-                                value={orderPrice} />
-                        </Flex>
-                        <Divider my={10} />
-                        <Flex justify={"space-between"}>
-                            <Text size="sm">Service charge:  </Text>
-                            <NumberFormatter
-                                className="text-sm"
-                                thousandSeparator
-                                prefix="₦"
-                                value={serviceCharge} />
-                        </Flex>
+                                        <Flex justify={"space-between"}>
+                                            <Text size="sm">Original cost:  </Text>
+                                            <NumberFormatter
+                                                className="text-sm"
+                                                thousandSeparator
+                                                prefix="₦"
+                                                value={orderPrice} />
+                                        </Flex>
+                                        <Divider my={10} />
+                                        <Flex justify={"space-between"}>
+                                            <Text size="sm">Service charge:  </Text>
+                                            <NumberFormatter
+                                                className="text-sm"
+                                                thousandSeparator
+                                                prefix="₦"
+                                                value={serviceCharge} />
+                                        </Flex>
 
-                        <Divider my={10} />
+                                        <Divider my={10} />
 
-                        <Flex justify={"space-between"}>
-                            <Text size="sm">Total:
-                            </Text>
-                            <NumberFormatter
-                                thousandSeparator
-                                prefix="₦"
-                                value={totalAmount} />
-                        </Flex>
-                    </Flex>
+                                        <Flex justify={"space-between"}>
+                                            <Text size="sm">Total:
+                                            </Text>
+                                            <NumberFormatter
+                                                thousandSeparator
+                                                prefix="₦"
+                                                value={totalAmount} />
+                                        </Flex>
+                                    </Flex>
 
-                    <Flex justify={"flex-end"} my={20}>
-                        <Button size="sm">
-                            Make Payment
-                        </Button>
-                    </Flex>
+                                    <PaystackConsumer
+                                        {...paystackConsumerProps}
+                                    >
+                                        {({ initializePayment }) => (
+                                            <Flex justify={"flex-end"} my={20}>
+                                                <Button size="sm"
+                                                    loading={loading}
+                                                    onClick={async () => {
+                                                        try {
+                                                            const res = await handlePayment()
+                                                            if (res) {
+                                                                initializePayment()
+                                                            }
+                                                        } catch (err) {
+                                                            setLoading(false)
+                                                            toast(err?.response?.data?.message, "Contact Support Support!").error()
+                                                        }
+                                                    }}>
+                                                    Make Payment
+                                                </Button>
+                                            </Flex>
+                                        )}</PaystackConsumer>
+                                </Flex>
+                                :
+                                <Flex my={20} direction={'column'}>
+                                    <Text fw={500} size={"xl"} ta={"center"} mb={10}>Hi there 👋</Text>
+                                    <Flex direction={"column"} my={20}>
+                                        <CheckCircle size={100} color="#228be6" className="mx-auto" />
+                                        <Text my={10}>This Order has been paid for already,
+                                            contact support if you have any complaint.</Text>
+                                    </Flex>
+                                </Flex>
+                            }
+                        </>
+                    }
+                </Flex>
+                <Flex
+                    mx={"auto"}
+                    maw={500} my={10} p={20}>
+                    <Text fw={300} size="sm">24/7 support via support@escrowsystem.com or +234 705 215 2823</Text>
                 </Flex>
             </Flex>
+
         </Flex>
     );
 }
